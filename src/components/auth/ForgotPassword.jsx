7bc2ff0524f616fa2c7c975ee87a9f02037a0b10
Mark <markthedev@gmail.com>
2022-08-01 06:08:23 -0500

    Hide links on forgot password for logged in users

--- src/components/auth/ForgotPassword.jsx
+++ src/components/auth/ForgotPassword.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../firebase/context';
 import useForm from '../../util/useForm';
 import validate from '../../util/validateForm';
 import Button from '../form/Button';
 import Input from '../form/Input';
 
 const ForgotPassword = () => {
+    const { user } = useContext(AuthContext);
+
     const { inputs, handleInputChange, handlePasswordResetSubmit, errors, resetError, resetSuccess } = useForm({
         email: ''
     }, validate);
@@ -70,12 +74,16 @@ const ForgotPassword = () => {
                                     </div>
                                 </div>
                             </form>
-                            <div className='row mb-3'>
-                                <div className='col'>
-                                    <p className='text-center fw-normal fs-6 text-muted'>Need an account? <Link to={process.env.REACT_APP_REGISTER_ROUTE}>Create your free account</Link>.</p>
-                                    <p className='text-center fw-normal fs-6 text-muted'>Already have an account? <Link to={process.env.REACT_APP_LOGIN_ROUTE}>Sign in</Link>.</p>
+                            {!user ?
+                                <div className='row mb-3'>
+                                    <div className='col'>
+                                        <p className='text-center fw-normal fs-6 text-muted'>Need an account? <Link to={process.env.REACT_APP_REGISTER_ROUTE}>Create your free account</Link>.</p>
+                                        <p className='text-center fw-normal fs-6 text-muted'>Already have an account? <Link to={process.env.REACT_APP_LOGIN_ROUTE}>Sign in</Link>.</p>
+                                    </div>
                                 </div>
-                            </div>
+                            :
+                                null
+                            }
                         </div>
                     </div>
                 </div>
